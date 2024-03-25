ead8f4f2da356a390185d5c9edd2acb82046a9ca
Jodie Mullins <jmullins2460@gmail.com>
2024-03-25 15:22:04 -0400

    Changed Price to Salary

--- src/CYPolling/products/models.py
+++ src/CYPolling/products/models.py
@@ -8,5 +8,5 @@ from django.db import models
 class Product(models.Model):
     title       = models.TextField()
     description = models.TextField()
-    price       = models.TextField()
+    salary      = models.TextField()
     rating      = models.TextField(default='3')
