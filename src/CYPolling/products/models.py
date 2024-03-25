4247790930b14f8ed48cc1c8727e3f6ec3be83dd
Jodie Mullins <jmullins2460@gmail.com>
2024-03-25 18:32:15 -0400

    database deletion nightmare warning

--- src/CYPolling/products/models.py
+++ src/CYPolling/products/models.py
@@ -8,6 +8,9 @@ from django.db import models
 # # Must:
 #  #        python manage.py makemigrations and python manage.py migrate when altered!!!! 
 
+# # -------------------------- DO NOT ---- DELETE DATABASE ------------------------------------------
+# # --------- DELETING DATABASE MAKES BAD THINGS HAPPEN AND TAKES THE SERVER WITH IT
+
 class Product(models.Model):
     title       = models.CharField(max_length=120)  # max length is  R E Q U I R E D 
     description = models.TextField(blank=True, null=True)
