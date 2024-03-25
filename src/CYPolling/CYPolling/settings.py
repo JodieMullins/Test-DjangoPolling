4c1fb4b57665bc759181d9699644173f3167f3ad
Jodie Mullins <jmullins2460@gmail.com>
2024-03-25 19:43:06 -0400

    start custom homepage

--- src/CYPolling/CYPolling/settings.py
+++ src/CYPolling/CYPolling/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # personally create apps:
     'products', 
+    'pages',   
 ]
 
 MIDDLEWARE = [
---
+++ src/CYPolling/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# a place to handle various web pages
+
+# Functions or classes
+
+# define home page
+def homepage_view():
+    return '<h1>Hello World</h1>'
